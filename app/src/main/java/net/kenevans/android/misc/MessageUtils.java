93167890e8bc8ce616955a1cf01723abe5b0ddfd
Kenneth Evans <ken@kenevans.net>
2017-01-02 12:20:27 -0500

    Added cursor.close(0 to getRawContactName.

--- app/src/main/java/net/kenevans/android/misc/MessageUtils.java
+++ app/src/main/java/net/kenevans/android/misc/MessageUtils.java
@@ -867,6 +867,7 @@ public class MessageUtils implements IConstants {
             // there should only be one
             break;
         }
+        nameCursor.close();
         return name;
     }
 
